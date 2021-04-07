f127649c14af91f922f7af8a5574e30a15a75a42
maxrabin <max.rabin17@gmail.com>
2021-04-07 13:18:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+/* const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); 
+
+*/
+
+
+
+function writeCards(names, event) {
+    const array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(num) {
+    let loop = num
+    while (loop >= 0) {
+        console.log(loop--);
+        
+    } 
+
+}
